5ef84d9342b8e8f515b9b2785f92d9ba609ee925
Teja <tejasimma033@gmail.com>
2024-07-08 17:16:07 +0530

    forecast query

--- client/src/predict-files.js
+++ client/src/predict-files.js
@@ -3,10 +3,6 @@ const API_URL = 'http://127.0.0.1:5000';
 export const forecastMaterial = async () => {
   const response = await fetch(`${API_URL}/forecast`, {
     method: 'POST',
-    // headers: {
-    //   'Content-Type': 'application/json',
-    // },
-    // body: JSON.stringify({ historicalData }),
   });
   return response.json();
 };
